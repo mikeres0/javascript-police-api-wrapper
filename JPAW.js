e6118b2e3a3b8c67cc98025be38d32e92dcdc658
Mike Resoli <mike.resoli@gmail.com>
2016-01-08 15:18:01 +0000

    Added various functions
    Added the following functions:
    
    testScript: test script is attached to HTML page correctly
    availableData: returns an object containing information on the available
    crime data, sorted by date
    getForces: returns an object containing details of each force
    getForceInfo: provides details information about a specific force
    getForceSeniors: returns personal and contact information for all senior
    offices of the specified force
    locateNeighbourhood: returns the neighbourhood code for the specified
    lat long coords

---
+++ JPAW.js
@@ -0,0 +1,79 @@
+//Test script connection
+function testScript(){
+	alert('javascript-police-api-wrapper is linked')
+}
+
+
+
+
+//Find available datasets
+function availableData(){
+	var response;
+	$.ajax({
+		url: "https://data.police.uk/api/crimes-street-dates",
+		dataType: "json",
+		success: function(data){
+			response = data;
+		}
+	})
+	return response;
+}
+
+
+
+
+//Gather force info
+function getForces(){
+	var response;
+	$.ajax({
+		url: "https://data.police.uk/api/forces",
+		dataType: "json",
+		success: function(data){
+			response = data;
+		}
+	})
+	return response;
+}
+
+function getForceInfo(force){
+	var response;
+	$.ajax({
+		url: "https://data.police.uk/api/forces/" + force,
+		dataType: "json",
+		success: function(data){
+			console.log(data)
+			//response = data;
+		}
+	})
+	return response;
+}
+
+function getForceSeniors(force){
+	var response;
+	$.ajax({
+		url: "https://data.police.uk/api/forces/" + force + "/people",
+		dataType: "json",
+		success: function(data){
+			console.log(data);
+			//response = data;
+		}
+	})
+	return response;
+}
+
+
+
+
+//Locate neighbourhood via lat long
+function locateNeighbourhood(lat, lon){
+	var response;
+	$.ajax({
+		url: "https://data.police.uk/api/locate-neighbourhood?q=" + lat + " ," + lon + "",
+		dataType: "json",
+		success: function(data){
+			response = data;
+		}
+	})
+	return response;
+}
+
